0daa776a99f8d80d1bb557b5bd5ea1ebd5f3484f
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-02-21 17:28:08 -0400

    Task 10: implement _atoi without forbidden headers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to convert
  *
- * Return: integer value found in the string, or 0 if none
+ * Return: the integer found in the string, or 0 if none
  */
 int _atoi(char *s)
 {
@@ -14,44 +13,29 @@ int _atoi(char *s)
 	int num = 0;
 	int started = 0;
 
-	/* Find sign(s) before the number */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
 			break;
-		i++;
-	}
-
-	/* Convert digits */
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-	{
-		int digit = s[i] - '0';
-
-		started = 1;
-
-		if (sign == 1)
-		{
-			/* Check overflow before: num = num * 10 + digit */
-			if (num > (INT_MAX - digit) / 10)
-				return (INT_MAX);
-			num = num * 10 + digit;
 		}
-		else
-		{
-			/* Build as negative to safely support INT_MIN */
-			/* Check underflow before: num = num * 10 - digit */
-			if (num < (INT_MIN + digit) / 10)
-				return (INT_MIN);
-			num = num * 10 - digit;
-		}
-
 		i++;
 	}
 
 	if (!started)
 		return (0);
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 - (s[i] - '0');
+		i++;
+	}
+
+	if (sign == 1)
+		num = -num;
 
 	return (num);
 }
