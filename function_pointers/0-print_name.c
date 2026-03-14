9abb5eeb8f1a7a4421b92ef53c6a819d441d6850
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-03-14 09:40:11 -0400

    Fix NULL include in print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 /**
  * print_name - prints a name using a function pointer
@@ -11,6 +12,5 @@ void print_name(char *name, void (*f)(char *))
 {
     if (name == NULL || f == NULL)
         return;
-
     f(name);
 }
