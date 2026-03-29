3237189e07bf94b941daed6309eb3ded993c555e
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-03-29 15:46:29 -0400

    Add read_textfile function and README for file I/O project

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters read and printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r_bytes;
+	ssize_t w_bytes;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	r_bytes = read(fd, buffer, letters);
+	if (r_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	if (w_bytes != r_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (w_bytes);
+}
---
+++ file_io/README.md
@@ -0,0 +1,29 @@
+# File I/O
+
+This project is part of the Holberton School low level programming curriculum.
+
+## Description
+
+This project introduces file descriptors and file input/output in C using system calls.
+
+## Learning Objectives
+
+At the end of this project, you should be able to explain:
+
+- How to create, open, close, read and write files
+- What file descriptors are
+- What the 3 standard file descriptors are and their POSIX names
+- How to use the system calls `open`, `read`, `write` and `close`
+- How to use the flags `O_RDONLY`, `O_WRONLY` and `O_RDWR`
+- What file permissions are
+- What a system call is
+- The difference between a function and a system call
+
+## Requirements
+
+- Allowed editors: `vi`, `vim`, `emacs`
+- All files are compiled on Ubuntu 20.04 LTS
+- Compilation flags:
+
+```bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89
