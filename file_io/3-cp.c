c4b7d6a2a611dd6f3db7af3f2cd7e1bd71661450
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-03-29 16:17:57 -0400

    Write cp program

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,14 +5,11 @@
 
 /**
  * close_fd - closes a file descriptor
- * @fd: file descriptor to close
+ * @fd: the file descriptor to close
  */
 void close_fd(int fd)
 {
-	int check;
-
-	check = close(fd);
-	if (check == -1)
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -29,7 +26,7 @@ void close_fd(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t rcount, wcount;
+	ssize_t letters_read, letters_written;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -45,33 +42,34 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close(fd_from);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_fd(fd_from);
 		exit(99);
 	}
 
-	rcount = read(fd_from, buffer, 1024);
-	while (rcount > 0)
+	letters_read = read(fd_from, buffer, 1024);
+	while (letters_read > 0)
 	{
-		wcount = write(fd_to, buffer, rcount);
-		if (wcount == -1 || wcount != rcount)
+		letters_written = write(fd_to, buffer, letters_read);
+		if (letters_written == -1 || letters_written != letters_read)
 		{
-			close(fd_from);
-			close(fd_to);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
 			exit(99);
 		}
-		rcount = read(fd_from, buffer, 1024);
+
+		letters_read = read(fd_from, buffer, 1024);
 	}
 
-	if (rcount == -1)
+	if (letters_read == -1)
 	{
-		close(fd_from);
-		close(fd_to);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
 		exit(98);
 	}
 
